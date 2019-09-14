3cf499bcb49ef41bc32a9716610bcfb8c6ddedac
Oleksandr Samohrai <sam_gray@ukr.net>
2019-09-14 11:52:07 +0300

    initial project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container maxWidth="sm">
+        <Typography
+          component="div"
+          style={{backgroundColor: '#cfe8fc'}}
         >
-          Learn React
-        </a>
-      </header>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={3}
+            style={{height: '100vh'}}
+          >
+            <Grid item xs={12}>
+              <Button variant="contained" color="primary">
+                login
+              </Button>
+            </Grid>
+          </Grid>
+        </Typography>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
