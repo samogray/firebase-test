fffff65baa9675a93c389890f60beb9ce870d0aa
Oleksandr Samohrai <sam_gray@ukr.net>
2019-09-14 12:06:24 +0300

    init firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
-import CssBaseline from '@material-ui/core/CssBaseline'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
+import firebase from './firebase'
 import './App.css'
 
-function App() {
+const App = () => {
+console.log(firebase.app())
   return (
     <div className="App">
       <Container maxWidth="sm">
-        <Typography
-          component="div"
-          style={{backgroundColor: '#cfe8fc'}}
-        >
+        <Typography component="div" style={{backgroundColor: '#cfe8fc'}}>
           <Grid
             container
             direction="row"
@@ -22,7 +20,7 @@ function App() {
             spacing={3}
             style={{height: '100vh'}}
           >
-            <Grid item xs={12}>
+            <Grid item xs={12} style={{textAlign: 'center'}}>
               <Button variant="contained" color="primary">
                 login
               </Button>
---
+++ src/firebase/index.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzIbYAMohzG2FYYrTJU0Ep-hNYd5B29XE",
+  authDomain: "react-auth-98197.firebaseapp.com",
+  databaseURL: "https://react-auth-98197.firebaseio.com",
+  projectId: "react-auth-98197",
+  storageBucket: "",
+  messagingSenderId: "849610762900",
+  appId: "1:849610762900:web:31ae15369e75e2750a2281"
+};
+
+
+firebase.initializeApp(firebaseConfig)
+
+
+export default firebase
+
+export const database = firebase.database()
+export const auth = firebase.auth()
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
